870ca0b32f612a5519581dafae4efbaf51dcbc78
Kumar Shahil <shahilsrivastava7@gmail.com>
2025-02-26 18:36:33 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import dotenv from "dotenv"
+import connectDB from "./utils/db.js"
+import userRoute from "./routes/user.route.js"
+import companyRoute from "./routes/company.route.js"
+import jobRoute from "./routes/job.route.js"
+import applicationRoute from "./routes/application.route.js"
 
-dotenv.config({});
+dotenv.config({})
 
 const app = express()
 
@@ -16,21 +16,23 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true })) // to support URL-encoded bodies
 app.use(cookieParser())
 
-
 const corsOptions = { origin: "http://localhost:5173", credentials: true }
 
 app.use(cors(corsOptions))
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!")
+})
 
 //api's
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job", jobRoute);
-app.use("/api/v1/application", applicationRoute);
-
+app.use("/api/v1/user", userRoute)
+app.use("/api/v1/company", companyRoute)
+app.use("/api/v1/job", jobRoute)
+app.use("/api/v1/application", applicationRoute)
 
 app.listen(PORT, () => {
-  connectDB();
+  connectDB()
   console.log(`Server is running on port ${PORT}`)
 })
