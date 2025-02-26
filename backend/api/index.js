a7df780555b2124b7d73f32680ef083edb9bd65f
Kumar Shahil <shahilsrivastava7@gmail.com>
2025-02-26 18:12:14 +0530

    Updated

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,17 +2,15 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
-import path from 'path';
+import connectDB from "../utils/db.js";
+import userRoute from "../routes/user.route.js";
+import companyRoute from "../routes/company.route.js";
+import jobRoute from "../routes/job.route.js";
+import applicationRoute from "../routes/application.route.js";
 
 dotenv.config({});
 
 const app = express()
-const __dirname=path.resolve()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) // to support URL-encoded bodies
@@ -32,8 +30,7 @@ app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 app.use(express.static(path.join(__dirname,'frontend/dist')))
-app.get('*',(req,res)=>{
-  res.sendFIle(path.join(__dirname,'frontend','dist','index.html'))
+
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`)
